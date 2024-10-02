b6b0fa3958611b8f2d6f6fac2996967e4a89612b
Zacharovanii <tuzudavid7@gmail.com>
2024-10-03 01:37:03 +0300

    done

--- Utils/expression.py
+++ Utils/expression.py
@@ -1,4 +1,4 @@
-from sympy import symbols, diff
+from sympy import symbols, diff, latex
 from sympy.parsing.sympy_parser import parse_expr
 
 
@@ -14,7 +14,13 @@ class Expression:
         b = -9
         lst = []
         if self.F(0) * self.F(1) < 0:
-            return 0, 1
+            f_a = float(self.F(0))
+            f_b = float(self.F(1))
+            ABLatex = [
+                f"f(0) = {f_a}",
+                f"f(1) = {f_b}"
+            ]
+            return 0, 1, ABLatex
         while a != 10:
             a += 1
             b += 1
@@ -23,16 +29,23 @@ class Expression:
         lst_filter = [abs(i[0] + i[1]) for i in lst]
         lst_min = lst_filter.index(min(lst_filter))
         res = lst[lst_min]
-        return res
+        a, b = res[0], res[1]
+        f_a = self.F(a)
+        f_b = self.F(b)
+        ABLatex = [
+            f"f({a}) = {f_a}",
+            f"f({b}) = {f_b}"
+        ]
+        return ABLatex
 
     @staticmethod
-    def chooseSegment(a, b, c, a_val, b_val, c_val, flag):
+    def chooseSegment(a, b, c, a_val, b_val, c_val, flag, l):
         a_hat = '+' if a_val >= 0 else '-'
         b_hat = '+' if b_val >= 0 else '-'
         c_hat = '+' if c_val >= 0 else '-'
-        a = str(a)[:8]
-        b = str(b)[:8]
-        c = str(c)[:8]
+        a = str(round(a, l))
+        b = str(round(b, l))
+        c = str(round(c, l))
         left = r'[\stackrel{' + a_hat + '}{' + a + r'}:\stackrel{' + c_hat + '}{' + c + '}]'
         right = r'[\stackrel{' + c_hat + '}{' + c + r'}:\stackrel{' + b_hat + '}{' + b + '}]'
         if flag == 'left':
@@ -59,6 +72,12 @@ class Expression:
     def getDiff2(self):
         return self.diffExpr_2
 
+    def getF1Latex(self):
+        return f"f^{r'{\prime}'} = {latex(self.diffExpr_1)}"
+
+    def getF2Latex(self):
+        return f"f^{r'{\prime\prime}'} = {latex(self.diffExpr_2)}"
+
 
 class ChordMethod:
     def __init__(self, expression: Expression, a, b, flag, eps=1e-2):
@@ -68,36 +87,40 @@ class ChordMethod:
         self.b = b
         self.eps = eps
         self.chordEqLatex = r'x = a - \frac{\left(b - a\right) f(a)}{f(b) - f(a)}'
+        self.afterCommaLen = len(str(eps))
         if flag == 'a':
             self.flagAB = 'a'
             self.c2F = lambda a, b, a_values, b_values: a - (a_values * (b - a)) / (b_values - a_values)
             self.chordEqLatex = r'x = a - \frac{\left(b - a\right) f(a)}{f(b) - f(a)}'
         elif flag == 'b':
             self.flagAB = 'b'
-            self.c2F = lambda a, b, a_values, b_values: b - (b_values * (a - b)) / (b_values - a_values)
+            self.c2F = lambda a, b, a_values, b_values: b - (b_values * (b - a)) / (b_values - a_values)
             self.chordEqLatex = r'x = b - \frac{\left(a - b\right) f(b)}{f(b) - f(a)}'
 
-    @staticmethod
-    def getChordEqLatex(a, b, a_val, b_val, x):
-        f_a = str(a_val)[:8]
+    def getChordEqLatex(self, a, b, a_val, b_val, x):
+        l = self.afterCommaLen
+        f_a = str(round(a_val, l))
         if a_val < 0:
             f_a = f"({f_a})"
-        f_b = str(b_val)[:8]
-        a = str(a)[:8]
-        b = str(b)[:8]
-        x = str(x)[:8]
-        chordLatex = f"x = {a}{r' - \frac{\left('}{b} - {a}{r'\right) '}{f_a}{'}{'}{f_b} - {f_a}{'}'} = {x}"
-        return chordLatex
-        # return 'x = ' + a + r' - \frac{\left(' + b + ' - ' + a + r'\right) ' + f_a + '}{' + f_b + ' - ' + f_a + '}'
+        f_b = str(round(b_val, l))
+        a = str(round(a, l))
+        b = str(round(b, l))
+        x = str(round(x, l))
+        if self.flagAB == 'a':
+            chordLatex = f"x = {a}{r' - \frac{\left('}{b} - {a}{r'\right) '}{f_a}{'}{'}{f_b} - {f_a}{'}'} = {x}"
+            return chordLatex
+        elif self.flagAB == 'b':
+            chordLatex = f"x = {b}{r' - \frac{\left('}{a} - {b}{r'\right) '}{f_a}{'}{'}{f_b} - {f_a}{'}'} = {x}"
+            return chordLatex
 
-    @staticmethod
-    def chooseSegment(a, b, c, a_val, b_val, c_val, flag):
+    def chooseSegment(self, a, b, c, a_val, b_val, c_val, flag):
+        l = self.afterCommaLen
         a_hat = '+' if a_val >= 0 else '-'
         b_hat = '+' if b_val >= 0 else '-'
         c_hat = '+' if c_val >= 0 else '-'
-        a = str(a)[:8]
-        b = str(b)[:8]
-        c = str(c)[:8]
+        a = str(round(a, l))
+        b = str(round(b, l))
+        c = str(round(c, l))
         left = r'[\stackrel{' + a_hat + '}{' + a + r'}:\stackrel{' + c_hat + '}{' + c + '}]'
         right = r'[\stackrel{' + c_hat + '}{' + c + r'}:\stackrel{' + b_hat + '}{' + b + '}]'
         if flag == 'left':
@@ -112,11 +135,14 @@ class ChordMethod:
         b = self.b
         eps = self.eps
         progress_lst = []
-        progress_lst.append([f"Идем\:по\:стороне: {self.flagAB}",
+        progress_lst.append([f"Идем{r'\:'}по{r'\:'}стороне: {self.flagAB}",
                              self.chordEqLatex])
         if F(a) * F(b) >= 0:
             raise Exception("Initial approximation error")
-        c1 = a
+        if self.flagAB == 'a':
+            c1 = a
+        elif self.flagAB == 'b':
+            c1 = b
         while True:
             a_values = float(F(a))
             b_values = float(F(b))
@@ -124,7 +150,7 @@ class ChordMethod:
             c2 = self.c2F(a, b, a_values, b_values)
             c_values = float(F(c2))
             if abs(c1 - c2) < eps:
-                return progress_lst, c2
+                return progress_lst, round(c1, self.afterCommaLen)
             if (a_values < 0 < c_values) or (a_values > 0 > c_values):
                 temp = [
                     self.getChordEqLatex(a, b, a_values, b_values, c2),
@@ -154,76 +180,100 @@ class NewtonMethod:
         self.eps = eps
         self.flagAB = None
         self.chooseABLatex = r'f(c) * f^{\prime\prime}(c) > 0'
+        self.afterCommaLen = len(str(eps))
 
     def chooseAB(self):
         aF = self.F(self.a)
         aF2 = self.F2(self.a)
         bF = self.F(self.b)
         bF2 = self.F2(self.b)
-        chooseLatex = [
-            'Если: c = a',
-            f'{float(aF)} * {float(aF2)} > 0',
-            'Если: c = b',
-            f'{float(bF)} * {float(bF2)} > 0'
-        ]
-        if self.F(self.a) * self.F2(self.a) >= 0:
+
+        if aF * aF2 >= 0:
+            side = r"Идем\:по\:стороне\:a"
             self.flagAB = 'a'
+            chooseLatex = [
+                self.chooseABLatex,
+                r'Если\:c\:=\:a:',
+                f'{float(aF)} * {float(aF2)} > 0',
+                r'Если\:c\:=\:b:',
+                f'{float(bF)} * {float(bF2)} > 0',
+                side,
+                r'x = c - \frac{f(c)}{f^{\prime}(c)}'
+            ]
+
             return self.a, chooseLatex
-        elif self.F(self.b) * self.F2(self.b) >= 0:
+
+        elif bF * bF2 >= 0:
+            side = r"Идем\:по\:стороне\:b"
             self.flagAB = 'b'
+            chooseLatex = [
+                self.chooseABLatex,
+                r'Если\:c\:=\:a:',
+                f'{float(aF)} * {float(aF2)} > 0',
+                r'Если\:c\:=\:b:',
+                f'{float(bF)} * {float(bF2)} > 0',
+                side,
+                r'x = c - \frac{f(c)}{f^{\prime}(c)}'
+            ]
+
             return self.b, chooseLatex
 
-
-    @staticmethod
-    def newtonFormulaLatex(c, cF, cF1, x):
-        c = str(c)[:7]
-        cF = str(cF)[:7]
-        cF1 = str(cF1)[:7]
-        x = str(x)[:7]
-        formulaLatex = r'x = ' + c + r' - \frac{' + cF + '}{' + cF1 + '}' + f" = {x}"
+    def newtonFormulaLatex(self, c, cF, cF1, x, iteration):
+        l = self.afterCommaLen
+        c = format(round(c, l), f'.{l}f').rstrip('0').rstrip('.')
+        cF = format(round(cF, l), f'.{l}f').rstrip('0').rstrip('.')
+        cF1 = format(round(cF1, l), f'.{l}f').rstrip('0').rstrip('.')
+        x = format(round(x, l), f'.{l}f').rstrip('0').rstrip('.')
+        formulaLatex = f"x{r'^{('}{iteration}{'}'} = {c} - {r'\frac{'}{cF}{'}{'}{cF1}{'}'} = {x}"
+        # formulaLatex = r'x = ' + c + r' - \frac{' + cF + '}{' + cF1 + '}' + f" = {x}"
         return formulaLatex
 
-    def getFlagAB(self):
-        return self.flagAB
+    def getFlagForChord(self):
+        if self.flagAB == 'a':
+            return 'b'
+        return 'a'
 
     def findRoot(self):
         x_0, latex = self.chooseAB()
-        print(x_0)
         a = self.a
         b = self.b
         progress_lst = []
         progress_lst.append(latex)
+        counter = 1
         while True:
             f = self.F(x_0)
             f1 = self.F1(x_0)
             x = x_0 - f/f1
             if (self.F(a) < 0 < self.F(x)) or (self.F(a) > 0 > self.F(x)):
                 temp = [
-                    self.newtonFormulaLatex(float(x_0), float(f), float(f1), float(x)),
+                    self.newtonFormulaLatex(float(x_0), float(f), float(f1), float(x), counter),
                     self.expr.chooseSegment(float(a), float(b), float(x), float(self.F(a)),
-                                            float(self.F(b)), float(self.F(x)), 'right')
+                                            float(self.F(b)), float(self.F(x)), 'right', self.afterCommaLen)
                 ]
                 progress_lst.append(temp)
                 b = x
 
             else:
                 temp = [
-                    self.newtonFormulaLatex(float(x_0), float(f), float(f1), float(x)),
+                    self.newtonFormulaLatex(float(x_0), float(f), float(f1), float(x), counter),
                     self.expr.chooseSegment(float(a), float(b), float(x), float(self.F(a)),
-                                            float(self.F(b)), float(self.F(x)), 'left')
+                                            float(self.F(b)), float(self.F(x)), 'left', self.afterCommaLen)
                 ]
                 progress_lst.append(temp)
                 a = x
 
             if abs(x - x_0) < self.eps:
-                return progress_lst, x
+                return progress_lst, round(x, self.afterCommaLen)
             x_0 = x
+            counter += 1
 
 
 if __name__ == "__main__":
-    f = Expression('2x**3 + 9x**2 - 4')
-    a, b = f.findAB()
-    res = NewtonMethod(f, a, b)
-    c, x = res.findRoot()
-    print(c)
+    f = Expression('x**3 + x**2 - 1')
+    # newton = NewtonMethod(f, a, b)
+    # newton_progress, newton_root = newton.findRoot()
+    # sideAB = newton.getFlagForChord()
+    # chord = ChordMethod(f, a, b, sideAB)
+    # chord_progress, chord_root = chord.findRoot()
+    print(f.findAB())
     # print(res.findRoot())
--- web_app.py
+++ web_app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, url_for
-from Utils.expression import *
+from os import getenv
+
+from dotenv import load_dotenv
+from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Optional
-from dotenv import load_dotenv
-from os import getenv
+
+from Utils.expression import *
 
 load_dotenv()
 app = Flask(__name__)
@@ -20,7 +22,7 @@ class FuncDataForm(FlaskForm):
                     validators=[Optional()])
     eps = StringField('Погрешность:',
                       validators=[DataRequired(message='Заполните все поля')],
-                      default='1e-6')
+                      default='1e-4')
     submit = SubmitField('Рассчитать!')
 
 
@@ -34,14 +36,26 @@ def multy_method():
     form = FuncDataForm()
     if form.validate_on_submit():
         expression = Expression(form.func.data)
-        a = float(form.a.data)
-        b = float(form.b.data)
+        F1 = expression.getF1Latex()
+        F2 = expression.getF2Latex()
+        a = form.a.data
+        b = form.b.data
+        if not a or not b:
+            a, b, ABLatex = expression.findAB()
+        else:
+            a, b, ABLatex = float(a), float(b), None
         eps = float(form.eps.data)
         try:
             newton = NewtonMethod(expression, a, b, eps=eps)
             newton_progress, newton_root = newton.findRoot()
-            return render_template('multy_method.html', newton_root=newton_root,
-                                   form=form, newton_progress=newton_progress)
+            sideAB = newton.getFlagForChord()
+            chord = ChordMethod(expression, a, b, sideAB, eps=eps)
+            chord_progress, chord_root = chord.findRoot()
+            return render_template('multy_method.html', ABLatex=ABLatex,
+                                   newton_root=newton_root, newton_progress=newton_progress,
+                                   chord_root=chord_root, chord_progress=chord_progress,
+                                   form=form, F1=F1, F2=F2
+                                   )
         except ValueError as e:
             return render_template('multy_method.html', error=e, form=form)
 
